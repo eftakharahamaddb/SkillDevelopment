c942119b4a59c9a5d040111e2edd8a8c7cf6129b
eftakharahamaddb <sendtoefty@gmail.com>
2018-03-28 16:55:52 +0530

    material_design_tool_bar

--- app/src/main/java/com/skill/develoment/skilldevelopment/AppBarCordinatorActivity.java
+++ app/src/main/java/com/skill/develoment/skilldevelopment/AppBarCordinatorActivity.java
@@ -1,8 +1,11 @@
 package com.skill.develoment.skilldevelopment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AppBarCordinatorActivity extends AppCompatActivity {
@@ -14,5 +17,24 @@ public class AppBarCordinatorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_app_bar_cordinator);
         toolBar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolBar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menus, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.next:
+                startActivity(new Intent(this, Drawer2Navigation.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/skill/develoment/skilldevelopment/MainActivity.java
+++ app/src/main/java/com/skill/develoment/skilldevelopment/MainActivity.java
@@ -1,6 +1,7 @@
 package com.skill.develoment.skilldevelopment;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -38,7 +39,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
-
+        switch (itemId) {
+            case R.id.next:
+                startActivity(new Intent(this, AppBarCordinatorActivity.class));
+        }
         return super.onOptionsItemSelected(item);
     }
 }
